04e3e8ff08cabc3ab9c843575edd6d0fed987a39
anaispcd <pcdanais@gmail.com>
2026-05-05 15:15:23 +0200

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -224,5 +224,4 @@ def manage_observation():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port):
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
